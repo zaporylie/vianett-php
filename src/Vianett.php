cd1bb87b79b9441b4c933530cbc2779319cee2f6
Jakub Piasecki <jakub@nymedia.no>
2016-12-07 10:34:30 +0100

    Remove trait

--- src/Vianett.php
+++ src/Vianett.php
@@ -3,7 +3,6 @@
 require_once 'Vianett/HttpRequestInterface.php';
 require_once 'Vianett/CurlInterface.php';
 require_once 'Vianett/VianettHttpRequest.php';
-require_once 'Vianett/CurlTrait.php';
 require_once 'Vianett/CurlGet.php';
 require_once 'Vianett/CurlPost.php';
 require_once 'Vianett/Client.php';
